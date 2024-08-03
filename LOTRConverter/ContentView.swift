7bb0dc79bfc4f5c7a623e319bc50fc2117f66351
iosdevyousra <eng1.yousra1@gmail.com>
2024-08-03 12:42:38 +0300

    define stacks !

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -5,4 +5,38 @@
 //  Created by M-Store on 8/2/24.
 //
 
+import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+          //background image
+            VStack{
+           //prancy pony image
+          //currency exchange text
+         //curency convention section
+                HStack{
+                 
+                //left conversion section
+                //equal sign
+                //right conversion section
+                    
+                    
+                    
+                }
+                
+                
+                
+            }
+            
+            
+            
+            
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
