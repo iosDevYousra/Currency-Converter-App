aa456eeb15bcac399229c7c651b818243c13a0b6
iosdevyousra <eng1.yousra1@gmail.com>
2025-08-24 22:41:43 +0300

    update !

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by M-Store on 8/24/25.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
