99117fa6bc1a99ba8d9cbbc22d4c041c375a12b4
iosdevyousra <eng1.yousra1@gmail.com>
2025-08-30 16:59:30 +0300

    amount field changed !

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -16,27 +16,27 @@ struct ContentView: View {
     @FocusState var rightTyping
     var body: some View {
         ZStack{
-          //background image
+            //background image
             Image(.background)
                 .resizable()
                 .ignoresSafeArea()
             VStack{
-           //prancy pony image
+                //prancy pony image
                 Image(.prancingpony)
                     .resizable()
                     .scaledToFit()
                     .frame(height: 200)
-            //currency exchange text
+                //currency exchange text
                 Text("Currency Exchange")
                     .font(.largeTitle)
                     .foregroundStyle(.white)
-         //curency convention section
+                //curency convention section
                 HStack{
-                //left conversion section
+                    //left conversion section
                     VStack{
-                      //curency
+                        //curency
                         HStack{
-                          //curency image
+                            //curency image
                             Image(leftCurrency.image)
                                 .resizable()
                                 .scaledToFit()
@@ -48,28 +48,24 @@ struct ContentView: View {
                         }
                         .padding(.bottom, -5)
                         .onTapGesture {
-                         showSelectCurrency.toggle()
+                            showSelectCurrency.toggle()
                         }
                         //text field
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
                             .focused($leftTyping)
-                            .onChange(of: leftAmount){
-                                if leftTyping {
-                                    rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
-                            }
-                        }
+                        
                     }
-                //equal sign
+                    //equal sign
                     Image(systemName: "equal")
                         .font(.largeTitle)
                         .foregroundColor(.white)
                         .symbolEffect(.pulse)
-                //right conversion section
+                    //right conversion section
                     VStack{
-                      //curency
+                        //curency
                         HStack{
-                          //curency text
+                            //curency text
                             Text(rightCurrency.name)
                                 .font(.headline)
                                 .foregroundStyle(.white)
@@ -88,18 +84,14 @@ struct ContentView: View {
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
                             .focused($rightTyping)
-                            .onChange(of: rightAmount){
-                                if rightTyping{
-                                    leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
-                            }
-                        }
+                          
                     }
                 }
                 .padding()
                 .background(.black.opacity(0.5))
                 .clipShape(.capsule)
                 Spacer()
-              //info button
+                //info button
                 HStack {
                     Spacer()
                     Button{
@@ -111,18 +103,37 @@ struct ContentView: View {
                             .font(.largeTitle)
                             .foregroundColor(.white)
                     }
+                    
                     .padding(.trailing)
-                    .sheet(isPresented: $showExhangeInfo){
-                        ExchangeInfo()
-                    }
-                    .sheet(isPresented: $showSelectCurrency){
-                        SelectCurrency(topCurrency: $leftCurrency, bottomCurrency: $rightCurrency)
-                    }
                 }
             }
         }
-    }
-}
+                    .onChange(of: leftAmount){
+                        if leftTyping {
+                            rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+                    }
+                }
+                    .onChange(of: rightAmount){
+                        if rightTyping{
+                            leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+                        }
+                    }
+                    .onChange(of: leftCurrency){
+                        leftAmount = rightCurrency.convert(rightAmount, to: leftCurrency)
+                    }
+                    .onChange(of: rightCurrency){
+                        rightAmount = leftCurrency.convert(leftAmount, to: rightCurrency)
+                    }
+                    .sheet(isPresented: $showExhangeInfo){
+                        ExchangeInfo()
+                    }
+        
+                    .sheet(isPresented: $showSelectCurrency){
+                        SelectCurrency(topCurrency: $leftCurrency, bottomCurrency: $rightCurrency)
+           }
+         }
+       }
+        
 #Preview {
     ContentView()
 }
